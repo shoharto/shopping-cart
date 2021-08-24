cdfc242a96941513dd1ae1df4d7ea7ce5476508a
shoharto <bayes@graduate.utm.my>
2021-08-24 20:51:22 +0800

    shopping cart handle decrease and update price

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-document.getElementById('case-plus').addEventListener('click', function () {
+function updateCaseNumber(isIncreasing) {
     const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
-    caseInput.value = (parseInt(caseNumber)) + 1;
-    console.log(caseNumber);
+    let caseNumber = caseInput.value;
+    if (isIncreasing) {
+        caseNumber = parseInt(caseNumber) + 1;
+    } else if (caseNumber > 0) {
+        caseNumber = parseInt(caseNumber) - 1;
+    }
+    caseInput.value = caseNumber;
+    //update case total
+
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseNumber * 59;
+}
+
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateCaseNumber(true)
 })
+
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateCaseNumber(false)
+});
